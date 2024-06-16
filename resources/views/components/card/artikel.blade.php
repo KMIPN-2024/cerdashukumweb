f4e67ec95640ef421ee49f5336367a2e2a768a99
roihan365 <bitcoiner.banjarmasin@gmail.com>
2024-06-16 11:34:33 +0800

    refactor responsive list article and list pasal

--- resources/views/components/card/artikel.blade.php
+++ resources/views/components/card/artikel.blade.php
@@ -1,21 +1,20 @@
 @props([
-    'title' => 'Title', 
-    'desc' => 'Description', 
-    'img' => 'assets/img/legal-article.png'
-    ])
+    'title' => 'Title',
+    'desc' => 'Description',
+    'img' => 'assets/img/legal-article.png',
+])
 
-<div>
-    <div class="">
-        <a href="#">
-            <img class="rounded-lg" src="{{ asset($img) }}" alt="" />
-        </a>
-    </div>
-    <div class="px-6 pb-6 pt-3">
-        <a href="#">
-            <h5
-                class="mb-1 text-xl font-bold whitespace-nowrap overflow-hidden overflow-ellipsis text-primary dark:text-white">{{ $title }}</h5>
-        </a>
-        <p class="mb-3 font-normal text-secondary line-clamp-2 overflow-hidden overflow-ellipsis dark:text-gray-400">
-            {{ $desc }}</p>
-    </div>
+<div class="">
+    <a href="#">
+        <img class="rounded-lg" src="{{ asset($img) }}" alt="" />
+    </a>
+</div>
+<div class="px-6 pb-6 pt-3">
+    <a href="#">
+        <h5
+            class="mb-1 text-xl font-bold line-clamp-1 overflow-hidden overflow-ellipsis text-primary dark:text-white">
+            {{ $title }}</h5>
+    </a>
+    <p class="mb-3 font-normal text-secondary line-clamp-2 overflow-hidden overflow-ellipsis dark:text-gray-400">
+        {{ $desc }}</p>
 </div>
