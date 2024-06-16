2ecee25477c2b330cfb2059a100a83d6ab580c50
roihan365 <bitcoiner.banjarmasin@gmail.com>
2024-06-16 14:43:28 +0800

    refactor validation input min 50 and max 250 word

--- app/Livewire/BantuanHukum.php
+++ app/Livewire/BantuanHukum.php
@@ -43,7 +43,7 @@ class BantuanHukum extends Component
         $this->loading = true;
 
         // Fetch data from the API
-        $getPasal = Http::timeout(60)->withoutVerifying()->post('https://cerdashukumapi-ktmv6bjp4q-as.a.run.app/get-pasal', [
+        $getPasal = Http::timeout(60)->post('https://cerdashukumapi-ktmv6bjp4q-as.a.run.app/get-pasal', [
             'sentence' => $this->input,
             'returnPasal' => true,
         ]);
